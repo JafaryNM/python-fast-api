9e951030b750f58b28e73e4d1e23e3443ae6d967
JAFARY <jaffarymdegela@gmail.com>
2025-09-13 12:31:16 +0300

    variable-python

---
+++ variable.py
@@ -0,0 +1,28 @@
+##### 
+# Variable is container to hold specific values 
+
+number =10
+tax_percentage = 0.3
+price = tax_percentage * number
+username = 'Jafary Frontend'
+first_name = 'Jafary'
+last_name ='Mdegela'
+
+print(username + " " +  first_name)
+print(price)
+
+
+# Re assigning values 
+
+first_number = 10
+second_number = 2
+
+print(first_number)
+print(second_number)
+
+second_number= 3
+
+print(first_number)
+print(second_number)
+
+
